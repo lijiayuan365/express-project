155d66b36af5c2dec21659f398e145fc1e2acb9f
lijiayuan365 <1399208311@qq.com>
2021-03-12 23:09:39 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+// tests
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
